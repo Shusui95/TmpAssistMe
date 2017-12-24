f023a260763dba8730975819a099ee86041f516f
Jeremy Marchand <jeremy.marchand@batiment.setec.fr>
2017-12-24 14:42:35 +0100

    Create a config file for global param

---
+++ config/config.js
@@ -0,0 +1,13 @@
+/**
+ *  API keys
+ * @type {string}
+ */
+const config = {
+    apiaiApp : "6f72b10d80c54b2a8000b1dac5ea449b",
+    WEATHER_API_KEY : "e5f8121716539c929cec4d955d207204",
+    defaultPort : 3987,
+};
+
+module.exports = config;
+
+
