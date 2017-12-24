e7ac77fae6fa282c2a1158f7f53c533749041ed6
Jeremy Marchand <jeremy.marchand@batiment.setec.fr>
2017-12-24 14:42:04 +0100

    Initiate hello world bot

---
+++ index.js
@@ -0,0 +1,42 @@
+const config = require('./config/config');
+const builder = require('botbuilder');
+const restify = require('restify');
+const apiairecognizer = require('api-ai-recognizer');
+const request = require('request');
+
+// Setup Restify Server
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || config.defaultPort, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+const apiaiApp = '6f72b10d80c54b2a8000b1dac5ea449b';
+const WEATHER_API_KEY = 'e5f8121716539c929cec4d955d207204';
+const defaultPort = 3987;
+
+
+/**
+ *  Create connector
+ * @type {ChatConnector}
+ */
+const connector = new builder.ChatConnector({
+    appId: process.env.APP_ID,
+    appPassword: process.env.APP_SECRET
+});
+/**
+ * Open an url
+ */
+server.post('/api/messages', connector.listen());
+
+/**
+ * Instanciate bot
+ * @type {UniversalBot}
+ */
+const bot = new builder.UniversalBot(connector, session => {
+    session.send("Hello world")
+    session.endDialog();
+});
+
+/**
+ * Enable conversation data persistence
+ */
+bot.set('persistConversationData', true);
