df27300b907659fe8ca2a69d59158999d3f6b30f
Jeremy Marchand <jeremy.marchand@batiment.setec.fr>
2018-01-13 16:43:08 +0100

    .

--- index.js
+++ index.js
@@ -36,6 +36,110 @@ server.get('/webhook', (req, res) => {
     }
 });
 
+// All callbacks for Messenger will be POST-ed here
+app.post("/webhook", (req, res) => {
+    // Make sure this is a page subscription
+    if (req.body.object === "page") {
+        // Iterate over each entry
+        // There may be multiple entries if batched
+        req.body.entry.forEach((entry) => {
+            // Iterate over each messaging event
+            entry.messaging.forEach((event) => {
+                if (event.postback) {
+                    processPostback(event);
+                } else if (event.message) {
+                    processMessage(event);
+                }
+            });
+        });
+        res.sendStatus(200);
+    }
+});
+
+function processMessage(event) {
+    if (!event.message.is_echo) {
+        let message = event.message;
+        let senderId = event.sender.id;
+
+        console.log("Received message from senderId: " + senderId);
+        console.log("Message is: " + JSON.stringify(message));
+
+        // You may get a text or attachment but not both
+        if (message.text) {
+            let formattedMsg = message.text.toLowerCase().trim();
+
+            // If we receive a text message, check to see if it matches any special
+            // keywords and send back the corresponding movie detail.
+            // Otherwise, search for new movie.
+            switch (formattedMsg) {
+                case "plot":
+                case "date":
+                case "runtime":
+                case "director":
+                case "cast":
+                case "rating":
+                    getMovieDetail(senderId, formattedMsg);
+                    break;
+
+                default:
+                    findMovie(senderId, formattedMsg);
+            }
+        } else if (message.attachments) {
+            sendMessage(senderId, {text: "Sorry, I don't understand your request."});
+        }
+    }
+}
+function processPostback(event) {
+    let senderId = event.sender.id;
+    let payload = event.postback.payload;
+
+    if (payload === "Greeting") {
+        // Get user's first name from the User Profile API
+        // and include it in the greeting
+        request({
+            url: "https://graph.facebook.com/v2.6/" + senderId,
+            qs: {
+                access_token: process.env.PAGE_ACCESS_TOKEN,
+                fields: "first_name"
+            },
+            method: "GET"
+        }, function(error, response, body) {
+            let greeting = "";
+            if (error) {
+                console.log("Error getting user's name: " +  error);
+            } else {
+                let bodyObj = JSON.parse(body);
+                name = bodyObj.first_name;
+                greeting = "Hi " + name + ". ";
+            }
+            let message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
+            sendMessage(senderId, {text: message});
+        });
+    } else if (payload === "Correct") {
+        sendMessage(senderId, {text: "Awesome! What would you like to find out? Enter 'plot', 'date', 'runtime', 'director', 'cast' or 'rating' for the various details."});
+    } else if (payload === "Incorrect") {
+        sendMessage(senderId, {text: "Oops! Sorry about that. Try using the exact title of the movie"});
+    }
+}
+
+// sends message to user
+function sendMessage(recipientId, message) {
+    request({
+        url: "https://graph.facebook.com/v2.6/me/messages",
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: "POST",
+        json: {
+            recipient: {id: recipientId},
+            message: message,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log("Error sending message: " + response.error);
+        }
+    });
+}
+
+
 /**
  * Instanciate bot
  * @type {UniversalBot}
