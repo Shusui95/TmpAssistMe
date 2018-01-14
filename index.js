8e252c31070fddaf1054c82c0faced926f98088c
Jeremy Marchand <jeremy.marchand@batiment.setec.fr>
2018-01-14 13:07:07 +0100

    .

--- index.js
+++ index.js
@@ -560,6 +560,65 @@ app.post('/webhook/', (req, res) => {
 
 });
 
+app.post('/ai/', (req, res) => {
+    try {
+        const data = JSONbig.parse(req.body);
+        console.log('data', data)
+        if (data.entry) {
+            let entries = data.entry;
+            entries.forEach((entry) => {
+                console.log('entry', entry)
+                let messaging_events = entry.messaging;
+                if (messaging_events) {
+                    messaging_events.forEach((event) => {
+                        if (event.message && !event.message.is_echo) {
+
+                            if (event.message.attachments) {
+                                let locations = event.message.attachments.filter(a => a.type === "location");
+
+                                // delete all locations from original message
+                                event.message.attachments = event.message.attachments.filter(a => a.type !== "location");
+
+                                if (locations.length > 0) {
+                                    locations.forEach(l => {
+                                        let locationEvent = {
+                                            sender: event.sender,
+                                            postback: {
+                                                payload: "FACEBOOK_LOCATION",
+                                                data: l.payload.coordinates
+                                            }
+                                        };
+
+                                        facebookBot.processFacebookEvent(locationEvent);
+                                    });
+                                }
+                            }
+
+                            facebookBot.processMessageEvent(event);
+                        } else if (event.postback && event.postback.payload) {
+                            if (event.postback.payload === "FACEBOOK_WELCOME") {
+                                facebookBot.processFacebookEvent(event);
+                            } else {
+                                facebookBot.processMessageEvent(event);
+                            }
+                        }
+                    });
+                }
+            });
+        }
+
+        return res.status(200).json({
+            status: "ok"
+        });
+    } catch (err) {
+        return res.status(400).json({
+            status: "error",
+            error: err
+        });
+    }
+
+});
+
 app.listen(REST_PORT, () => {
     console.log('Rest service ready on port ' + REST_PORT);
 });
